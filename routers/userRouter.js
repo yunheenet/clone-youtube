dacc1cf3ce36131a47cf3faa572d79bbc0bd4d1c
YunHee Lee <yunheenet@gmail.com>
2019-06-22 01:41:19 +0900

    MVC Pattern

---
+++ routers/userRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import routes from "../routes";
+import { users, editProfile, changePassword, userDetail } from "../controllers/userController";
+
+const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+
+export default userRouter;
