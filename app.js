f86bf5be6bf3cdce7446509e147d1934fd7571d1
YunHee Lee <yunheenet@gmail.com>
2019-06-21 23:34:23 +0900

    Routing

--- app.js
+++ app.js
@@ -3,29 +3,24 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import { userRouter } from "./router";
 
 const app = express()
 
-const PORT = 4000;
-
-const handleListening = () =>
-    console.log(`Listening on : http://localhost:${PORT}`);
-
 const handleHome = (req, res) => res.send("Hello from my ass");
 
-// JS Arrow function
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-// Middleware
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-// Routing
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
