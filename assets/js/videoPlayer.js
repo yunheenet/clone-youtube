2b8732625628f9766705c8f4f45f4bb63483c634
YunHee Lee <yunheenet@gmail.com>
2019-07-19 22:18:14 +0900

    ADD API Registering a View part two

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -7,7 +7,15 @@ const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("jsVolume");
 
+const registerView = () => {
+  const videoId = window.location.href.split("/videos/")[1];
+  fetch(`/api/${videoId}/view`, {
+    method: "POST"
+  });
+};
+
 function handlePlayClick() {
+  videoPlayer.currentTime = videoPlayer.duration - 10;
   if (videoPlayer.paused) {
     videoPlayer.play();
     playBtn.innerHTML = '<i class="fas fa-pause"></i>';
@@ -87,6 +95,7 @@ function setTotalTime() {
 }
 
 function handleEnded() {
+  registerView();
   videoPlayer.currentTime = 0;
   playBtn.innerHTML = '<i class="fas fa-play"></i>';
 }
