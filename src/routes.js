ea1915a7cc5f8a42e9cd22eb4bf13e68c23c1360
YunHee Lee <yunheenet@gmail.com>
2019-07-26 18:55:58 +0900

    MOD routes

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const LOGOUT = "/logout";
 const SEARCH = "/search";
 
 // Users
+
 const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
@@ -13,6 +14,7 @@ const CHANGE_PASSWORD = "/change-password";
 const ME = "/me";
 
 // Videos
+
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
@@ -20,14 +22,17 @@ const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
 // Github
+
 const GITHUB = "/auth/github";
 const GITHUB_CALLBACK = "/auth/github/callback";
 
 // Facebook
+
 const FB = "/auth/facebook";
 const FB_CALLBACK = "/auth/facebook/callback";
 
 // API
+
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
 const ADD_COMMENT = "/:id/comment";
